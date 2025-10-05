000e01a8c429dd23d618bc14eb14bb7ca68f2b99
premchand505 <prem595pop@gmail.com>
2025-10-05 22:03:26 +0530

    fix: use dynamic port from environment variable for deployment

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
